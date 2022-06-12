d42d4a8d5196362f51d56b8d254f55cf18c87bb1
polina993 <>
2022-06-12 18:10:25 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+    while(number %   >= 1)
+       {
+           int digit1 = number % 10;
+           int digit2 = number / 10;
+           int digit3 = digit2 /10;
+       }
+       Console.WriteLine(digit3);
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
