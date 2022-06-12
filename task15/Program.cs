b992d5f7348fa3eaec480bcf61119396870f7425
polina993 <>
2022-06-12 18:39:48 +0300

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите число от 1 до 7:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number < 6)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
