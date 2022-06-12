b716ad59023875ff371fe51ddc7e865f2edae33d
polina993 <>
2022-06-12 17:14:57 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+    int digit1 = number / 10;
+    int digit2 = digit1 % 10;
+    Console.WriteLine(digit2);
+}
+else 
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
